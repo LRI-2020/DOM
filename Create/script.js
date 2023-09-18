71c1eccb4113d7b75e1737869a91148ff036cae2
LaylaR <layla.richard.2111@gmail.com>
2023-09-18 13:14:45 +0200

    exercices DOM

---
+++ Create/script.js
@@ -0,0 +1,83 @@
+﻿let learners = ["Alexandre Va", "Alexandre Ve", "Antoine", "Bastien", "Carole", "Dorian", "Elisabeth", "Elodie", "Justin", "Justine F", "Justine M","Kimi", "Layla", "Lidwine","Lucas","Marie","Mathias","Okly","Pierre","Robin","Rosalie","Stephane","Tim","Tom","Valentin","Virginie"];
+let sections = [];
+
+
+for(let learner of learners){    
+    
+    let currentSection = createSection(learner);    
+    let para = createParagraph(learner);
+    
+    para.style.color = isLightColor(currentSection.style.backgroundColor)? "black" :"white";
+     
+    currentSection.appendChild(para);    
+    sections.push(currentSection);
+    sections.sort((a, b) => 0.5 - Math.random());
+    
+}
+
+for(let section of sections){
+    
+    let parentNode = document.querySelector("article");
+    parentNode.appendChild(section);
+}
+
+//functions
+function createSection(learner) {
+    let section = document.createElement("section");
+    section.setAttribute("id", learner);
+    section.style.backgroundColor = createRdmRgb();
+    return section;
+}
+
+function createParagraph(learner) {
+    let para = document.createElement("p");
+    para.innerText = learner;
+    return para;
+}
+
+function isLightColor(color){
+
+    return computeBrightness(color) > 127.5;
+}
+
+
+function rdm(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function createRdmRgb(){
+
+    let r = rdm(0,256);
+    let g = rdm(0,256);
+    let b = rdm(0,256);
+
+    return `rgb(${r},${g},${b})`;
+}
+
+function computeBrightness(color){
+
+    let rgb = rgbToObj(color);
+    //sqrt( .299 R2 + .587 G2 + .114 B2 )
+    return Math.sqrt(0.299 * (Math.pow(rgb.red,2)) + 0.587 * (Math.pow(rgb.green,2)) + 0.114 * (Math.pow(rgb.blue,2)) )
+}
+
+
+function rgbToObj(rgb) {
+
+    let colors = ["red", "green", "blue", "alpha"]
+
+    let colorArr = rgb.slice(
+        rgb.indexOf("(") + 1,
+        rgb.indexOf(")")
+    ).split(", ");
+
+    let obj = {};
+
+    colorArr.forEach((k, i) => {
+        obj[colors[i]] = k
+    })
+
+    return obj;
+}
---
+++ Introduction/script.js
@@ -0,0 +1,36 @@
+﻿let title= (document.getElementsByTagName("title")[0]);
+console.log(title.innerText);
+
+title.innerText = "Modifying the DOM";
+
+let body=document.body;
+
+// let color= "#FF69B4";
+
+
+let color= createRdmRgb();
+body.style.backgroundColor = color;
+
+for (let child of body.children){
+    
+    console.log(child.nodeName);
+}
+function rdm(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function createRdmRgb(){
+    
+    let r = rdm(0,256);
+    let g = rdm(0,256);
+    let b = rdm(0,256);
+    
+    return `rgb(${r},${g},${b})`;
+}
+
+// changeBackground(color);
+// function changeBackground(color) {
+//     document.body.style.background = color;
+// }
