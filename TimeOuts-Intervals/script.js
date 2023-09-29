f370603ca21d88ae04205e7f0e68e95e0caa6700
LaylaR <layla.richard.2111@gmail.com>
2023-09-29 10:59:09 +0200

    Exercise 1 interval

---
+++ TimeOuts-Intervals/script.js
@@ -0,0 +1,21 @@
+﻿
+
+let p = document.createElement('p');
+p.classList.add("section-p__ExerciseText_Exercise1");
+document.querySelector(".section__Exercise1").appendChild(p);
+
+let text = "Keller";
+let array = text.split("");
+
+
+let count = 0;
+
+
+const i = setInterval(function(){
+    p.innerText+=array[count];
+    count++;
+    
+    if(count === array.length){
+        clearInterval(i);
+    }
+},1000);
