889b14f999d3e338660c0743540faffff80ebaee
LaylaR <layla.richard.2111@gmail.com>
2023-10-02 10:53:28 +0200

    Exercise 3

--- Dates/Exercise3/Exercise3.js
+++ Dates/Exercise3/Exercise3.js
@@ -1,19 +1,17 @@
 ﻿
-let input = Number(prompt("Enter a number of hours : "));
-displayDateTime(input);
+
+let input = document.querySelector("input#hours");
+
+input.addEventListener('keyup',function(){
+    displayDateTime(input.value);
+})
+
+
 
 function displayDateTime(hours) {
-    let currentElement = createElement(`futureDateIn${hours}Hours`);
+    let currentElement = document.getElementById(`futureDateTime`);
     currentElement.innerText = `In ${hours} hours, we will be the ${GetDateTime(hours)}`
 }
-
-function createElement(title) {
-    let element = document.createElement('p');
-    element.classList.add("displayText__" + title);
-    document.querySelector('body').appendChild(element);
-    return element;
-}
-
 function GetDateTime(hours) {
 
     return new Date(Date.now() + (hours * 3600 * 1000));
