a150ffdfe3e65d441ec5101e8b9bae052eee8dc0
LaylaR <layla.richard.2111@gmail.com>
2023-10-02 10:34:03 +0200

    Dates - EX 1 and 2

---
+++ Dates/Exercise1/Exercise1.js
@@ -0,0 +1,23 @@
+﻿//Exercice1
+
+displayDateTimeForCity("Brussels","Europe/Brussels");
+displayDateTimeForCity("Anchorage","America/Anchorage");
+displayDateTimeForCity("Reykjavik","Atlantic/Reykjavik");
+displayDateTimeForCity("SaintPetersburg","Europe/Moscow");
+
+function displayDateTimeForCity(city, timeZone){
+    let currentCity = createElementForTimeZone(city);
+    setInterval(function(){
+        currentCity.innerText = `The current Time at ${city} is : ${getCurrentTimeForTZ(timeZone)}`
+    },1000)
+}
+
+function createElementForTimeZone(city){
+   let element = document.createElement('p');
+   element.classList.add("tzDisplay__"+city);
+   document.querySelector('body').appendChild(element);
+   return element;
+}
+function getCurrentTimeForTZ(timeZone) {
+    return new Date(Date.now()).toLocaleString(undefined, {timeZone: timeZone});
+}
---
+++ Dates/Exercise2/Exercise2.js
@@ -0,0 +1,30 @@
+﻿//Days Since my BirthDate
+let refDate = new Date(1989, 10, 21);
+displayNumberOfDays(refDate);
+
+//Days since any date
+
+let input = (prompt("Enter a date with the format : yyyy,mm,dd")).split(',').map(i => Number(i));
+let date = new Date(input[0], input[1] - 1, input[2]);
+
+displayNumberOfDays(date);
+
+function displayNumberOfDays(refDate, element) {
+    let currentElement = createElement("");
+    currentElement.innerText = `There are  ${GetDays(refDate)} days passed since ${refDate}`
+}
+
+function createElement(title) {
+    let element = document.createElement('p');
+    element.classList.add("displayText__" + title);
+    document.querySelector('body').appendChild(element);
+    return element;
+}
+
+function GetDays(refDate) {
+
+    let now = new Date(Date.now());
+    return Math.floor(Math.abs(now - refDate) / (1000 * 60 * 60 * 24));
+
+}
+
