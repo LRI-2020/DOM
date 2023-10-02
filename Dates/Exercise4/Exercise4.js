1971050f2961ec79cf348c636b22caca31b199a7
LaylaR <layla.richard.2111@gmail.com>
2023-10-02 12:12:06 +0200

    Exercise 4

---
+++ Dates/Exercise4/Exercise4.js
@@ -0,0 +1,23 @@
+﻿
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+
+
+let dayName = document.querySelector(".cardText__dayName");
+let dayNumber = document.querySelector(".cardText__dayNumber");
+let month = document.querySelector(".cardText__month");
+let year = document.querySelector(".cardText__year");
+let time = document.querySelector(".timeText");
+
+setInterval(function(){
+    let now = new Date(Date.now());
+    dayName.innerText = days[now.getDay()].toUpperCase();
+    dayNumber.innerText = now.getDay();
+    month.innerText= months[now.getMonth()].toUpperCase();
+    year.innerText= now.getFullYear();
+    time.innerText=`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+},1000)
+
+
+
+
