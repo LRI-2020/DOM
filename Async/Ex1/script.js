83d82e4f17a9ccb35b5d4327a74b81b8113e230d
LaylaR <layla.richard.2111@gmail.com>
2023-10-06 09:22:52 +0200

    Ex1 async

---
+++ Async/Ex1/script.js
@@ -0,0 +1,16 @@
+﻿let btn = document.querySelector(".generatesData");
+
+btn.addEventListener('click', () =>
+    fetch('./data.json').then((response) => response.json())
+        .then(res => LoadData(res)));
+
+function LoadData(data){
+    let list = document.createElement('ul');
+    for (let item of data){
+        let li = document.createElement('li');
+        li.innerText = item;
+        list.appendChild(li);
+    }
+    
+    document.querySelector('body').appendChild(list);
+}
